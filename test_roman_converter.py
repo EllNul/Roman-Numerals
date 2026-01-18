47e2278ed495823bf8f955a844f061a772fabd5f
EllNul <elliot.pacey-carrier@education.gov.uk>
2026-01-18 23:43:53 +0000

    test okay!

--- test_roman_converter.py
+++ test_roman_converter.py
@@ -2,11 +2,13 @@ import unittest
 
 from roman_converter import int_to_roman, roman_to_int
 
-def test_int_to_roman_(self):
-    self.assertEqual(int_to_roman(73), "LXXIII")
+class TestRomanConverter(unittest.TestCase):
 
-def test_roman_to_int(self):
-    self.assertEqual(roman_to_int("MMMDCXXV"), 3625)
+    def test_int_to_roman_(self):
+        self.assertEqual(int_to_roman(73), "LXXIII")
+
+    def test_roman_to_int(self):
+        self.assertEqual(roman_to_int("MMMDCXXV"), 3625)
 
 if __name__ == "__main__":
     unittest.main()
