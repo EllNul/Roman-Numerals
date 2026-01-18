0c2e37e87e6f17ea549eb5331bd6af0b07dc2bb7
EllNul <elliot.pacey-carrier@education.gov.uk>
2026-01-18 22:17:14 +0000

    correct testing

--- test_roman_converter.py
+++ test_roman_converter.py
@@ -5,7 +5,10 @@ from roman_converter import num_to_roman,roman_to_num
 class TestRomanCoverter(unittest.TestCase):
 
     def test_num_to_roman_basic(self):
-        self.asserEqual(num_to_roman(73), "LXXIII")
+        self.assertEqual(num_to_roman(73), "LXXIII")
+
+    def test_roman_to_num_basic(self):
+        self.assertEqual(roman_to_num("DLXI"), 561)
 
 if __name__ == "__Main__":
     unittest.main()
