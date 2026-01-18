0f04b460c07e70dfa6019fa20ac6e8d42baeaf38
EllNul <elliot.pacey-carrier@education.gov.uk>
2026-01-18 23:37:30 +0000

    addtion of test

--- test_roman_converter.py
+++ test_roman_converter.py
@@ -1,8 +1,10 @@
 import unittest
 
-class SmokeTest(unittest.TestCase):
-    def test2_and2(self):
-        self.assertEqual(2+2,4)
+def test_int_to_roman_(self):
+    self.assertEqual(int_to_roman(73), "LXXIII")
+
+def test_roman_to_int(self):
+    self.assertEqual(roman_to_int("MMMDCXXV"), 3625)
 
 if __name__ == "__main__":
     unittest.main()
