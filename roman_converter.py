c8467ef8a12b68ac697ffda3d40899274fac250d
EllNul <elliot.pacey-carrier@education.gov.uk>
2026-01-18 21:38:28 +0000

    roman convert set up

---
+++ roman_converter.py
@@ -0,0 +1,8 @@
+
+import roman
+
+def num_to_roman(number):
+    return roman.toRoman(number)
+
+def roman_to_num(text):
+    return roman.fromRoman(text)
--- test_roman_converter.py
+++ test_roman_converter.py
@@ -1,11 +1,3 @@
 
 
 import unittest
-
-
-class SmokeTest(unittest.TestCase):
-    def test2_and2(self):
-        self.assertEqual(2 + 2, 4)
-
-if __name__ == '__main__':
-    unittest.main()
