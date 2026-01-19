75a9506656a6e97e309189dc401a705d46318dc5
EllNul <elliot.pacey-carrier@education.gov.uk>
2026-01-19 00:40:18 +0000

    exceptions

--- test_roman_converter.py
+++ test_roman_converter.py
@@ -9,6 +9,14 @@ class TestRomanConverter(unittest.TestCase):
     def test_roman_to_int(self):
         self.assertEqual(roman_to_int("MMMDCCXCII"), 3792)
 
+    def test_invalid_roman_to_int(self):
+        with self.assertRaises(Exception):
+            roman_to_int("ABC")
+
+    def test_invalid_int_to_roman(self):
+        with self.assertRaises(Exception):
+            roman_to_int("40000000000000000000000")
+
 
 if __name__ == "__main__":
      unittest.main()
