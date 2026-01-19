4b6b1e6002e9b6961ecbc164e42e139441d1bdcd
EllNul <elliot.pacey-carrier@education.gov.uk>
2026-01-19 00:32:54 +0000

    intro test to roman

--- test_roman_converter.py
+++ test_roman_converter.py
@@ -1,8 +1,13 @@
 import unittest
 
-class SmokeTest(unittest.TestCase): 
-    def test2_and2(self):
-            self.assertEqual(2+2,4)
+class TestRomanConverter(unittest.TestCase):
+
+    def test_int_to_roman(self):
+        self.assertEqual(int_to_roman(84), "LXXXIV")
+
+    def test_roman_to_int(self):
+        self.assertEqual(roman_to_int("MMMDCCXCII"), 3792)
+
 
 if __name__ == "__main__":
      unittest.main()
